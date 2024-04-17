edc2c52780142fcb778796f8af0ae8f029e70a8f
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2024-04-17 20:18:27 +0200

    cosmetic changes

--- book1/ch03/data_types/src/main.rs
+++ book1/ch03/data_types/src/main.rs
@@ -30,6 +30,7 @@ fn main() {
 
     // let tup: (i32, f64, u8) = (500, 6.4, 1);
     // let (x, y, z) = tup;
+    // println!("{}", tup.1);
     // println!("{x}, {y}, {z}");
     // let c = tup.0;
     // println!("tup 0 is {c}");
