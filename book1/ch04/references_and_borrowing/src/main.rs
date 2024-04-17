01e3e789cc7ec25f13b0529cbba5fbcf2ba3d7bd
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2024-04-17 20:04:54 +0200

    added book1/ch04/references_and_borrowing

---
+++ book1/ch04/references_and_borrowing/src/main.rs
@@ -0,0 +1,30 @@
+fn main() {
+    // let m1 = String::from("Hello");
+    // let m2 = String::from("world");
+    // greet(m1, m2);
+    // println!("{} {}", m1, m2); // Error: m1 and m2 are moved
+
+    // let m1 = String::from("Hello");
+    // let m2 = String::from("world");
+    // let (m1_again, m2_again) = greet2(m1, m2);
+    // println!("{} {}", m1_again, m2_again);
+
+    // let m1 = String::from("Hello");
+    // let m2 = String::from("world");
+    // greet3(&m1, &m2); // note the ampersands
+    // println!("{} {}", m1, m2);
+}
+
+// fn greet(g1: String, g2: String) {
+//     println!("{} {}", g1, g2);
+// }
+
+// fn greet2(g1: String, g2: String) -> (String, String) {
+//     println!("{} {}", g1, g2);
+//     (g1, g2)
+// }
+
+// fn greet3(g1: &String, g2: &String) {
+//     // note ampersands
+//     println!("{} {}", g1, g2);
+// }
