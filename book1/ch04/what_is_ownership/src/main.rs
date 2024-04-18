c27ea5fd6a16d7b96345925945aa65a0d2397d43
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2024-04-18 20:11:08 +0200

    adjusted book1/ch04/what_is_ownership for official doc.rust-lang.org

--- book1/ch04/what_is_ownership/src/main.rs
+++ book1/ch04/what_is_ownership/src/main.rs
@@ -1,32 +1,64 @@
-// fn read(y: bool) {
-//     if y {
-//         println!("y is true!");
-//     }
-// }
+fn main() {
+    // let s = "hello"; // string literal
+    // println!("{}", s);
 
-fn add_suffix(mut name: String) -> String {
-    name.push_str(" Jr.");
-    name
+    // String type to be put on the heap at runtime
+    // let s = String::from("hello");
+    // println!("{}", s);
+
+    // let mut s = String::from("hello");
+    // s.push_str(", world!"); // appends a literal to a String
+    //                         // Strings can be mutated but literals cannot
+    // println!("{}", s);
+
+    // let x = 5;
+    // let y = x; // two 5s put (copied) on the stack
+    // println!("{} {}", x, y);
+
+    // let s1 = String::from("hello");
+    // let s2 = s1;
+    // // println!("{}", s1); // error, s2 is the owner
+    // println!("{}", s2); // s1 was moved into s2
+
+    // let s1 = String::from("hello");
+    // let s2 = s1.clone(); // like (deep?)copy from other programming languages
+    // println!("s1 = {}, s2 = {}", s1, s2);
+
+    // The mechanics of passing a value to a function are similar to
+    // those when assigning a value to a variable
+    // let s = String::from("hello");
+    // takes_ownership(s); // cannot use s after that line
+    // let x = 5;
+    // makes_copy(x); // can use x after that line
+
+    // let s1 = gives_ownership();
+    // let s2 = String::from("hello");
+    // let s3 = takes_and_gives_back(s2); // cannot use s2 after that line
+    // println!("{}, {}", s1, s3);
+
+    let s1 = String::from("hello");
+    let (s2, len) = calculate_length(s1);
+    println!("The length of '{}' is {}.", s2, len);
 }
 
-fn main() {
-    // safe program
-    // let x = true;
-    // read(x);
+// fn takes_ownership(some_string: String) {
+//     println!("{}", some_string);
+// }
 
-    // unsafe program, using x before it's defined
-    // check at compile time
-    // read(x);
-    // let x = true;
+// fn makes_copy(some_integer: i32) {
+//     println!("{}", some_integer);
+// }
 
-    // let first = String::from("Ferris");
-    // let full = add_suffix(first);
-    // println!("{full}");
-    // cannot use
-    // println!("{full}, originally {first}"); // value moved to full
+// fn gives_ownership() -> String {
+//     let some_string = String::from("yours");
+//     some_string
+// }
 
-    let first = String::from("Ferris");
-    let first_clone = first.clone();
-    let full = add_suffix(first_clone);
-    println!("{full}, originally {first}");
+// fn takes_and_gives_back(a_string: String) -> String {
+//     a_string
+// }
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
+    (s, length)
 }
