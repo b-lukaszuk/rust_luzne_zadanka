aaf2a24a9a7b83ca9d98002790cdea18fad9310e
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2024-04-21 09:03:11 +0200

    cosmetic changes

--- book1/ch03/functions/src/main.rs
+++ book1/ch03/functions/src/main.rs
@@ -2,38 +2,69 @@
 //     println!("Another function");
 // }
 
+// fn main() {
+//     println!("Hello, world!");
+//     another_function();
+// }
+
 // fn another_function(x: i32) {
+//     println!("In another function",);
 //     println!("The value of x is: {}", x);
+// }
+
+// fn main() {
+//     another_function(5);
 // }
 
 // fn print_labeled_measurement(value: i32, unit_label: char) {
 //     println!("The measurement is: {}{}", value, unit_label);
 // }
 
+// fn main() {
+//     print_labeled_measurement(5, 'h')
+// }
+
+// fn main() {
+//     let y = 6; // statement
+//     println!("{}", y);
+// }
+
+// fn main() {
+//     let x = (let y = 6); // error, cannot assign value of a statement
+//     println!("{}", y);
+// }
+
+// fn main() {
+//     let y = {
+//         let x = 3;
+//         x + 1 // expression
+//     };
+//     println!("The value of y is: {}", y);
+// }
+
 // fn five() -> i32 {
-//     5
+//     5 // expression, returns 5
+// }
+
+// fn main() {
+//     let x = five();
+//     println!("The value of x is: {}", x);
 // }
 
 fn plus_one(x: i32) -> i32 {
-    x + 1 // ; - will make it a statement and produce error
+    x + 1
 }
 
 fn main() {
-    // println!("Hello, world!");
-
-    // another_function();
-    // another_function(5);
-    // print_labeled_measurement(5, 'h');
-    // let y = 6;
-    // let x = (let y = 6); // error
-
-    // let y = {
-    //     let x = 3;
-    //     x + 1
-    // };
-    // println!("The value of y is: {}", y);
-
-    // let x = five();
     let x = plus_one(5);
     println!("The value of x is: {}", x);
 }
+
+// fn plus_one(x: i32) -> i32 {
+//     x + 1; // this is a statement, nothing to be returned
+// }
+
+// fn main() {
+//     let x = plus_one(5);
+//     println!("The value of x is: {}", x);
+// }
--- book1/ch03/if_expressions/src/main.rs
+++ book1/ch03/if_expressions/src/main.rs
@@ -1,34 +1,57 @@
+// fn main() {
+//     let number = 3;
+//     if number < 5 {
+//         println!("condition was true");
+//     } else {
+//         println!("condition was false");
+//     }
+// }
+
+// fn main() {
+//     let number = 7;
+//     if number < 5 {
+//         println!("condition was true");
+//     } else {
+//         println!("condition was false");
+//     }
+// }
+
+// fn main() {
+//     let number = 3;
+//     if number {
+//         // error the condition isn't bool
+//         println!("number was three");
+//     }
+// }
+
+// fn main() {
+//     let number = 3;
+//     if number != 0 {
+//         println!("number was something other than zero");
+//     }
+// }
+
+// fn main() {
+//     let number = 6;
+//     if number % 4 == 0 {
+//         println!("number is divisible by 4");
+//     } else if number % 3 == 0 {
+//         println!("number is divisible by 3");
+//     } else if number % 2 == 0 {
+//         println!("number is divisible by 2");
+//     } else {
+//         println!("number is not divisible by 4, 3, or 2");
+//     }
+// }
+
 fn main() {
-    // let number = 3;
-    // let number = 7;
-
-    // if number < 5 {
-    //     println!("condition was true");
-    // } else {
-    //     println!("condition was false");
-    // }
-
-    // if number {
-    //     println!("number was seven");
-    // }
-
-    // if number != 0 {
-    //     println!("number was something other than zero");
-    // }
-
-    // if number % 4 == 0 {
-    //     println!("number is divisible by 4");
-    // } else if number % 3 == 0 {
-    //     println!("number is divisible by 3");
-    // } else if number % 2 == 0 {
-    //     println!("number is divisible by 2");
-    // } else {
-    //     println!("number is not divisible by 4, 3, or 2");
-    // }
-
     let condition = true;
     let number = if condition { 5 } else { 6 };
-    // let number = if condition { 5 } else { "six" }; // type mismatch, so error
-
-    println!("The value of number is: {}", number);
+    println!("The value of number is: {number}");
 }
+
+// fn main() {
+//     let condition = true;
+//     let number = if condition { 5 } else { "six" }; // error, type instability
+//     println!("The value of number is: {number}");
+// }
