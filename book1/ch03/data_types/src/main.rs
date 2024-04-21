eafafa14b67f3042b618b0b40b7904d5136dc14d
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2024-04-21 08:32:36 +0200

    cosmetic changes

--- book1/ch03/data_types/src/main.rs
+++ book1/ch03/data_types/src/main.rs
@@ -1,41 +1,113 @@
+// fn main() {
+//     let guess = "42".parse().expect("Not a number!"); // error no clear type
+//     println!("Guess is {guess}");
+// }
+
+// fn main() {
+//     let guess: u32 = "42".parse().expect("Not a number!");
+//     println!("Guess is {guess}");
+// }
+
+// fn main() {
+//     let x = 2.0; // f64
+//     let y: f32 = 3.0; //f32
+//     println!("x is {x}, y is {y}");
+// }
+
+// fn main() {
+//     let sum = 5 + 10;
+//     println!("sum 5 + 10 is {sum}");
+//     let difference = 95.5 - 4.3;
+//     println!("difference 95.5 - 4.3 is {difference}");
+//     let product = 4 * 30;
+//     println!("product 4 * 30 is {product}");
+//     let quotient = 56.7 / 32.2;
+//     println!("quotient 56.7 / 32.2 is {quotient}");
+//     let truncated = -5 / 3;
+//     println!("truncated -5 / 3 is {truncated}");
+//     let remainder = 43 % 5;
+//     println!("remaineder 43 % 5 is {remainder}");
+// }
+
+// fn main() {
+//     let t = true;
+//     let f: bool = false; // with explicit type annotation
+//     println!("{t} {f}");
+// }
+
+// fn main() {
+//     let c = 'z';
+//     let z: char = 'ℤ'; // with explicit type annotation
+//     let heart_eyed_cat = '😻';
+//     println!("{c} {z} {heart_eyed_cat}");
+// }
+
+// fn main() {
+//     let tup: (i32, f64, u8) = (500, 6.4, 1);
+//     println!("{}", tup); // error, no instance of show for tup
+// }
+
+// fn main() {
+//     let tup: (i32, f64, u8) = (500, 6.4, 1);
+//     let (x, y, z) = tup; // error unused variables x and z
+//     println!("The value of y is: {}", y);
+// }
+
+// fn main() {
+//     let tup: (i32, f64, u8) = (500, 6.4, 1);
+//     let (_, y, _) = tup;
+//     println!("The value of y is: {}", y);
+// }
+
+// fn main() {
+//     let x: (i32, f64, u8) = (500, 6.4, 1);
+//     let five_hundred = x.0;
+//     let six_point_four = x.1;
+//     let one = x.2;
+//     println!("{}, {}, {}", five_hundred, six_point_four, one);
+// }
+
+// fn main() {
+//     let a = [1, 2, 3, 4, 5];
+//     println!("{}", a); // error, no show type for a
+// }
+
+// fn main() {
+//     let a = [1, 2, 3, 4, 5];
+//     println!("{:?}", a);
+// }
+
+// fn main() {
+//     let a: [i32; 5] = [1, 2, 3, 4, 5]; // explicit [type, len] declaration
+//     println!("{:?}", a);
+// }
+
+// fn main() {
+//     let a: [i32; 5] = [1, 2, 3, 4, 5]; // explicit [type, len] declaration
+//     println!("First element of array a is: {}", a[0]);
+// }
+
+use std::io;
+
 fn main() {
-    // let guess: u32 = "42".parse().expect("Not a number!");
-    // println!("Guess is {guess}");
+    let a = [1, 2, 3, 4, 5];
+    println!("Please enter an array index.");
 
-    // let x = 2.0; // f64
-    // let y: f32 = 3.0; //f32
-    // println!("x is {x}, y is {y}");
+    let mut index = String::new();
 
-    // let sum = 5 + 10;
-    // println!("sum 5 + 10 is {sum}");
-    // let difference = 95.5 - 4.3;
-    // println!("difference 95.5 - 4.3 is {difference}");
-    // let product = 4 * 30;
-    // println!("product 4 * 30 is {product}");
-    // let quotient = 56.7 / 32.2;
-    // println!("quotient 56.7 / 32.2 is {quotient}");
-    // let truncated = -5 / 3;
-    // println!("truncated -5 / 3 is {truncated}");
-    // let remainder = 43 % 5;
-    // println!("remaineder 43 % 5 is {remainder}");
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
 
-    // let t = true;
-    // let f: bool = false;
-    // println!("{t} {f}");
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
 
-    // let c = 'z';
-    // let z: char = 'Z'; // with explicity type annotation
-    // let sth = '😻';
-    // println!("{c} {z} {sth}");
+    let element = a[index]; // if index is our of a bounds, then thread panic
 
-    // let tup: (i32, f64, u8) = (500, 6.4, 1);
-    // let (x, y, z) = tup;
-    // println!("{}", tup.1);
-    // println!("{x}, {y}, {z}");
-    // let c = tup.0;
-    // println!("tup 0 is {c}");
-
-    // let a = [1, 2, 3, 4, 5];
-    let a: [i32; 5] = [1, 2, 3, 4, 5];
-    println!("a = {}", a[0]);
+    println!(
+        "The value of the element at index {} is: {}",
+        index, element
+    );
 }
--- book1/ch03/variables/src/main.rs
+++ book1/ch03/variables/src/main.rs
@@ -1,25 +1,40 @@
+// fn main() {
+//     let x = 5;
+//     println!("The value of x is: {x}");
+//     x = 6; // error, cannot change immutable variable
+//     println!("The value of x is: {x}");
+// }
+
+// fn main() {
+//     let mut x = 5;
+//     println!("The value of x is: {x}");
+//     x = 6;
+//     println!("The value of x is: {x}");
+// }
+
+// fn main() {
+//     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+//     println!("Number of seconds in three hours: {THREE_HOURS_IN_SECONDS}")
+// }
+
+// fn main() {
+//     let x = 5;
+//     let x = x + 1;
+//     {
+//         let x = x * 2;
+//         println!("The value of x in the inner scope is: {x}");
+//     }
+//     println!("The value of x is: {x}");
+// }
+
+// fn main() {
+//     let mut spaces = "       ";
+//     spaces = spaces.len(); // error, changed type of spaces
+//     println!("The length of spaces is: {spaces}")
+// }
+
 fn main() {
-    // let mut x = 5;
-    // println!("The value of x is: {x}");
-    // x = 6;
-    // println!("The value of x is: {x}");
-
-    // const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
-    // println!("Number of seconds in three hours: {THREE_HOURS_IN_SECONDS}")
-
-    // let x = 5;
-    // let x = x + 1;
-    // {
-    //     let x = x * 2;
-    //     println!("The value of x in the inner scope is: {x}");
-    // }
-    // println!("The value of x is: {x}");
-
     let spaces = "       ";
-    let spaces = spaces.len();
+    let spaces = spaces.len(); // no error, changed type but shadowing
     println!("The length of spaces is: {spaces}")
-
-    // let mut spaces = "   ";
-    // spaces = spaces.len(); // error
-    // println!("The length of spaces is: {spaces}")
 }
