9ab542f5bb1141b1c77f4da7f2fdd39b7182711c
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2024-05-02 15:26:59 +0200

    modified (added paths and referring to an item)

--- book1/ch07/restaurant/src/lib.rs
+++ book1/ch07/restaurant/src/lib.rs
@@ -1,6 +1,6 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {
+    pub mod hosting {
+        pub fn add_to_waitlist() {
             println!("Adding to waitlist.");
         }
         fn seat_at_table() {
@@ -20,4 +20,59 @@ mod front_of_house {
             println!("Taking payment");
         }
     }
+}
+
+pub fn eat_at_restaurant1() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+}
+
+fn deliver_order() {
+    println!("Delivering an order.");
+}
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+    fn cook_order() {
+        println!("Cooking order");
+    }
+}
+
+fn eat_at_restaurant2() {
+    // Order a breakfast in the smmer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd liek
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
+    //
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
