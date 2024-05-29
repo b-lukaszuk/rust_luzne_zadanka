8e8edeec604a876733a4d54a3d37a6fb33e02ca6
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2024-05-29 19:54:52 +0200

    splitting code into a library crate

--- book1/ch12/minigrep/src/main.rs
+++ book1/ch12/minigrep/src/main.rs
@@ -1,33 +1,7 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
 
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            Err("not enough arguments")
-        } else {
-            let query = args[1].clone();
-            let file_path = args[2].clone();
-
-            Ok(Config { query, file_path })
-        }
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("\nWith text:\n\n{}", contents);
-
-    Ok(())
-}
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -40,7 +14,7 @@ fn main() {
     println!("\nSearching for '{}'", config.query);
     println!("In file '{}'", config.file_path);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
     }
