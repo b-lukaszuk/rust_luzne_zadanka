4ebe22ddc0d6fe653a0fb8e1e91f087a5a9802a3
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2024-07-08 20:50:23 +0200

    added book1/ch16/mutex_example2

---
+++ book1/ch16/mutex_example2/src/main.rs
@@ -0,0 +1,24 @@
+use std::sync::Mutex;
+use std::thread;
+
+fn main() {
+    let counter = Mutex::new(0);
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            // cannot move the ownership of counter into multiple threads
+            // still, move of the ownership of counter to a single thread is OK
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
+}
