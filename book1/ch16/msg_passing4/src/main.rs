621a188612ee3fe8c2c491e8ad00c2ab74881de6
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2024-07-08 19:18:05 +0200

    added book1/ch16/msg_passing4

---
+++ book1/ch16/msg_passing4/src/main.rs
@@ -0,0 +1,42 @@
+use std::sync::mpsc; // mpsc - multiple producer, single consumer
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = tx.clone(); // antoher transmitter/sender
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+        // didn't drop the val, e.g 3-4 lines below
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
