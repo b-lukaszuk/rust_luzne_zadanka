f075d20fb542f2dcd8b91a7b0d0752c55dbd75ca
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2024-07-29 18:22:55 +0200

    added book1/ch17/traits3/

---
+++ book1/ch17/traits3/src/lib.rs
@@ -0,0 +1,32 @@
+pub mod gui {
+    pub trait Draw {
+        fn draw(&self);
+    }
+
+    pub struct Screen {
+        pub components: Vec<Box<dyn Draw>>,
+    }
+
+    impl Screen {
+        pub fn run(&self) {
+            for component in self.components.iter() {
+                component.draw()
+            }
+        }
+    }
+
+    pub struct Button {
+        pub width: u32,
+        pub height: u32,
+        pub label: String,
+    }
+
+    impl Draw for Button {
+        fn draw(&self) {
+            println!(
+                "Drawing button {}x{} '{}'",
+                self.width, self.height, self.label
+            )
+        }
+    }
+}
