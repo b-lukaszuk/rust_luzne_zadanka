35cd535362caa2b84f8509b44da915cbb575f4e7
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-01-14 18:22:49 +0100

    added book3/ch01/comments.rs

---
+++ book3/ch01/comments.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // println!("Hellow, world!");
+    /*
+     * println!("This is a multiline comment");
+     */
+
+    let x = 5 + /* 90 +  */ 5;
+    println!("Is `x` 10 or 100? x = {}", x);
+}
