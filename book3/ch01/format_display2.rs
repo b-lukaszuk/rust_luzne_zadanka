bb0a73090ed8661f7a649cac4001838734008337
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-01-16 21:16:48 +0100

    added Display/activity to book3/ch01/format_display2.rs

--- book3/ch01/format_display2.rs
+++ book3/ch01/format_display2.rs
@@ -21,6 +21,18 @@ impl fmt::Display for Point2D {
     }
 }
 
+#[derive(Debug)]
+struct Complex {
+    real: f64,
+    imag: f64,
+}
+
+impl fmt::Display for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} + {}i", self.real, self.imag)
+    }
+}
+
 fn main() {
     let minmax = MinMax(0, 14);
     println!("Compare strucures:",);
@@ -40,6 +52,14 @@ fn main() {
     println!("Compare points:");
     println!("Display: {}", point);
     println!("Debug: {:?}", point);
+
+    let compl = Complex {
+        real: 3.3,
+        imag: 7.2,
+    };
+    println!("Compare complex numbers:");
+    println!("Display: {}", compl);
+    println!("Debug: {:?}", compl);
 
     // Erro. Both `Debug` and `Display` were implemented but `{:b}`
     // requires `fmt::Binary` to be implemented. This will not work.
