141b9e06ef21afa4dcc41956669ed008d43ccb03
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-01-23 21:05:15 +0100

    added activity1 to book3/ch02/structs/structs.rs

--- book3/ch02/structs/structs.rs
+++ book3/ch02/structs/structs.rs
@@ -13,14 +13,26 @@ struct Unit;
 struct Pair(i32, f32);
 
 // struct with two fields
+#[derive(Debug)]
 struct Point {
     x: f32,
     y: f32,
 }
 
+#[derive(Debug)]
 struct Rectangle {
     top_left: Point,
     bottom_right: Point,
+}
+
+fn get_area(r: Rectangle) -> f32 {
+    let Rectangle {
+        top_left: tl,
+        bottom_right: br,
+    } = r;
+    let height = (tl.y - br.y).abs();
+    let width = (br.x - tl.x).abs();
+    height * width
 }
 
 fn main() {
@@ -49,13 +61,15 @@ fn main() {
         y: top_edge,
     } = point;
 
-    let _rectangle = Rectangle {
+    let rectangle = Rectangle {
         top_left: Point {
             x: left_edge,
             y: top_edge,
         },
         bottom_right: bottom_right,
     };
+    println!("\n{:#?}", rectangle);
+    println!("Area: {:.2} [cm^2]\n", get_area(rectangle));
 
     // instantiate a unit struct
     let _unit = Unit;
