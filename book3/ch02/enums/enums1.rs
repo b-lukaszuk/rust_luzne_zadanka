15e34cbace42266bf140a97f3a6c4e9685073218
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-01-24 19:52:34 +0100

    added book3/ch02/enums/enums1.rs

---
+++ book3/ch02/enums/enums1.rs
@@ -0,0 +1,37 @@
+enum WebEvent {
+    // `enum` may be `unit-like`,
+    PageLoad,
+    PageUnload,
+    // or tuple struct,
+    KeyPress(char),
+    Paste(String),
+    // or c-like structure
+    Click { x: i64, y: i64 },
+}
+
+fn inspect(event: WebEvent) {
+    match event {
+        WebEvent::PageLoad => println!("page loaded"),
+        WebEvent::PageUnload => println!("page unloaded"),
+        WebEvent::KeyPress(c) => println!("pressed '{}'.", c),
+        WebEvent::Paste(s) => println!("pasted \"{}\".", s),
+        WebEvent::Click { x, y } => {
+            println!("clicked at x={}, y={}.", x, y);
+        }
+    }
+}
+
+fn main() {
+    let pressed = WebEvent::KeyPress('x');
+    // `to_owned()` creates an owned `String` from a string slice.
+    let pasted = WebEvent::Paste("my text".to_owned());
+    let click = WebEvent::Click { x: 20, y: 80 };
+    let load = WebEvent::PageLoad;
+    let unload = WebEvent::PageUnload;
+
+    inspect(pressed);
+    inspect(pasted);
+    inspect(click);
+    inspect(load);
+    inspect(unload);
+}
