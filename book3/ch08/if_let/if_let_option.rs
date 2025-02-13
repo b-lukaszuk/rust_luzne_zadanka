20cb1c8b58096a3d8e4bb888b8cb9c05f8878f7c
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-02-13 17:17:20 +0100

    added book3/ch08/if_let/if_let_option.rs

---
+++ book3/ch08/if_let/if_let_option.rs
@@ -0,0 +1,28 @@
+fn main() {
+    let number = Some(7);
+    let letter: Option<i32> = None;
+    let emoticon: Option<i32> = None;
+
+    if let Some(i) = number {
+        println!("Matched {:?}!", i);
+    }
+
+    if let Some(i) = letter {
+        println!("Matched {:?}!", i);
+    } else {
+        // case: destructure failed
+        println!("Didn't match a number. Let's go with a letter!");
+    }
+
+    let i_like_letters = false;
+
+    if let Some(i) = emoticon {
+        println!("Matched {:?}!", i);
+    // alternate failure branch
+    } else if i_like_letters {
+        println!("Didn't match a number. Let's go with a letter!");
+    // final failure case
+    } else {
+        println!("I don't like letters. Let's go with an emoticon :)!");
+    }
+}
