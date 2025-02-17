5e73cb76163455850564a5362b5358bc8f3d1dab
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-02-17 16:13:14 +0100

    added book3/ch09/functions/fizz_buzz.rs

---
+++ book3/ch09/functions/fizz_buzz.rs
@@ -0,0 +1,31 @@
+// there's no restriction on the order of function definitions
+fn main() {
+    fizzbuzz_to(100);
+}
+
+fn is_divisible_by(lhs: u32, rhs: u32) -> bool {
+    if rhs == 0 {
+        return false;
+    }
+    lhs % rhs == 0
+}
+
+// functions that "don't" return a value, actually return the unit type `()`
+fn fizzbuzz(n: u32) -> () {
+    if is_divisible_by(n, 15) {
+        println!("fizzbuzz");
+    } else if is_divisible_by(n, 3) {
+        println!("fizz");
+    } else if is_divisible_by(n, 5) {
+        println!("buzz");
+    } else {
+        println!("{}", n);
+    }
+}
+
+// a function returns `()`, return type can be omitted from signature
+fn fizzbuzz_to(n: u32) {
+    for n in 1..=n {
+        fizzbuzz(n);
+    }
+}
