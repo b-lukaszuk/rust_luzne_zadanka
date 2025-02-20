f8cb98b2ef61bfc08d22a9ca7b7159358ac6d0e0
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-02-20 21:00:37 +0100

    added book3/ch09/output_params/example.rs

---
+++ book3/ch09/output_params/example.rs
@@ -0,0 +1,30 @@
+// anonymous closure types are unknown,
+// so we have to use 'impl Trait' to return them
+fn create_fn() -> impl Fn() {
+    let text = "Fn".to_owned();
+
+    // 'move' signals that all captures occur by value
+    move || println!("This is a: {}", text)
+}
+
+fn create_fnmut() -> impl FnMut() {
+    let text = "FnMut".to_owned();
+
+    move || println!("This is a: {}", text)
+}
+
+fn create_fnonce() -> impl FnOnce() {
+    let text = "FnOnce".to_owned();
+
+    move || println!("This is a: {}", text)
+}
+
+fn main() {
+    let fn_plain = create_fn();
+    let mut fn_mut = create_fnmut();
+    let fn_once = create_fnonce();
+
+    fn_plain();
+    fn_mut();
+    fn_once();
+}
