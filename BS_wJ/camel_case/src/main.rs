54065f1a3afcc30e2c19c14598c92420a4146001
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-06-05 19:25:32 +0200

    added BS_wJ/camel_cas/*/main.rs with initial contents

--- BS_wJ/camel_case/src/main.rs
+++ BS_wJ/camel_case/src/main.rs
@@ -1,3 +1,20 @@
+fn print_intro() {
+    println!("Toy program for the camel case problem.");
+    println!("Warning. It may or may not work correctly.\n");
+}
+
+fn print_outro() {
+    println!("\nThat's all. Goodbye!");
+}
+
+fn print_solution() {
+    println!("The solution goes here.");
+}
+
 fn main() {
-    println!("Hello, world!");
+    print_intro();
+
+    print_solution();
+
+    print_outro();
 }
