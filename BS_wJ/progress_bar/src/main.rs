7c49f9ac60610b40230267814910a229785ac9c2
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-06-07 19:45:20 +0200

    added init content to BS_wJ/progress_bar/*/main.rs

--- BS_wJ/progress_bar/src/main.rs
+++ BS_wJ/progress_bar/src/main.rs
@@ -1,3 +1,20 @@
+fn print_intro() {
+    println!("Toy program for the progress bar problem.");
+    println!("Warning. It may or may not work correctly.\n");
+}
+
+fn print_outro() {
+    println!("\nThat's all. Goodbye!");
+}
+
+fn print_solution() {
+    println!("The examples go here.");
+}
+
 fn main() {
-    println!("Hello, world!");
+    print_intro();
+
+    print_solution();
+
+    print_outro();
 }
