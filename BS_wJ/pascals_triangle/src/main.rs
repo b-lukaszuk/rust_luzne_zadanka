7d319c61c1b3c314fed4735e46eff5c4348936ef
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-06-11 19:03:27 +0200

    added initial scaffolding to BS_wJ/pascals_triangle/*/main.rs

--- BS_wJ/pascals_triangle/src/main.rs
+++ BS_wJ/pascals_triangle/src/main.rs
@@ -1,3 +1,20 @@
+fn print_intro() {
+    println!("Toy program.");
+    println!("It displays Pascal's triangles (it may or may not work correctly).");
+}
+
+fn print_solution() {
+    println!("\nThe solution goes here.");
+}
+
+fn print_outro() {
+    println!("\nThat's all. Goodbye!");
+}
+
 fn main() {
-    println!("Hello, world!");
+    print_intro();
+
+    print_solution();
+
+    print_outro();
 }
