0d64939b2b4167230a4cf9c045fac94db39b66bb
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-06-11 19:44:07 +0200

    changed i32 to u32 in BS_wJ/pascals_triangle/

--- BS_wJ/pascals_triangle/src/main.rs
+++ BS_wJ/pascals_triangle/src/main.rs
@@ -1,23 +1,23 @@
-fn get_sum_of_pairs(row: &Vec<i32>) -> Vec<i32> {
+fn get_sum_of_pairs(row: &Vec<u32>) -> Vec<u32> {
     assert!(row.len() > 1);
-    let mut result: Vec<i32> = vec![];
+    let mut result: Vec<u32> = vec![];
     for i in 0..row.len() - 1 {
         result.push(row[i] + row[i + 1]);
     }
     result
 }
 
-fn get_row(prev_row: &Vec<i32>) -> Vec<i32> {
+fn get_row(prev_row: &Vec<u32>) -> Vec<u32> {
     assert!(prev_row.len() > 1);
-    let mut result: Vec<i32> = get_sum_of_pairs(prev_row);
+    let mut result: Vec<u32> = get_sum_of_pairs(prev_row);
     result.insert(0, 1);
     result.push(1);
     result
 }
 
-fn get_pascal_triangle(n: u32) -> Vec<Vec<i32>> {
+fn get_pascal_triangle(n: u32) -> Vec<Vec<u32>> {
     assert!(0 < n && n < 11);
-    let mut triangle: Vec<Vec<i32>> = vec![vec![1], vec![1, 1]];
+    let mut triangle: Vec<Vec<u32>> = vec![vec![1], vec![1, 1]];
     for i in 2..=n as usize {
         triangle.push(get_row(&triangle[i - 1]));
     }
@@ -26,7 +26,7 @@ fn get_pascal_triangle(n: u32) -> Vec<Vec<i32>> {
 
 fn print_pasc_traingle(n: u32) {
     assert!(0 < n && n < 11);
-    let triangle: Vec<Vec<i32>> = get_pascal_triangle(n);
+    let triangle: Vec<Vec<u32>> = get_pascal_triangle(n);
     for r in triangle {
         println!("{:?}", r);
     }
