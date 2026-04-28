d2018b05a1ca2aa56d2a9856069ab35c4e72a5e9
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2026-04-28 21:28:16 +0200

    cleanup and fresh start

--- book3/ch01/comments/comments.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    // println!("Hellow, world!");
-    /*
-     * println!("This is a multiline comment");
-     */
-
-    let x = 5 + /* 90 +  */ 5;
-    println!("Is `x` 10 or 100? x = {}", x);
-}
